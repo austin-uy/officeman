9f569e7e6085065ef63bcc897ab8c1049ded1f16
Norvylle Austin Uy <austin.uy@adish.co.jp>
2019-09-17 15:28:56 +0800

    added temporary flag for Admin user

--- app/helpers/equipment_helper.rb
+++ app/helpers/equipment_helper.rb
@@ -1,6 +1,10 @@
 module EquipmentHelper
   def get_equipment(user_id)
     return nil unless user_id
-    @equipment = Equipment.where(user_id: user_id).order(:id).page(params[:page]).per(10)
+    if current_user.role.eql? "admin" #admin
+      @equipment = Equipment.order(:id).page(params[:page]).per(10)
+    else
+      @equipment = Equipment.where(user_id: user_id).order(:id).page(params[:page]).per(10)
+    end
   end
 end
