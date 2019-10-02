0b11a94eadf6c350446a39f0836269230ac29bcc
Norvylle Austin Uy <austin.uy@adish.co.jp>
2019-10-02 14:54:32 +0800

    removed commented line on index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,6 @@ class QuestionsController < ApplicationController
   # GET /questions.json
   def index
     @questions = Question.all
-    # @questions = Question.order(:id).page(params[:page]).per(3)
   end
 
   # GET /questions/1
