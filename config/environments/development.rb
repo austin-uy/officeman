75fed790cf70d85a608aa450f863d56553ae2af6
Norvylle Austin Uy <austin.uy@adish.co.jp>
2019-10-25 15:01:09 +0800

    Forgot Password doesn't work
    - Added configuration for smtp integration with SendGrid
    - Set devise error messages to be put on alert box
    - Edited placement of errors
    - Edited email templates
    - Added local_env.yml to .gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,24 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: ENV["USERNAME"] }
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => ENV["SMTP_ADDRESS"],
+      :port                 => 587,
+      :domain               => 'localhost',
+      :user_name            => ENV["USERNAME"],
+      :password             => ENV["PASSWORD"],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
