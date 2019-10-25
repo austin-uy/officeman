144b8523c20137d96da1c2abe5db718895521827
Norvylle Austin Uy <austin.uy@adish.co.jp>
2019-10-25 11:27:35 +0800

    removed duplicate js triggers

--- app/assets/javascripts/equipment.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $(document).ready(function(){
-    let search = new URLSearchParams(document.location.search)
-    if(search.get("open") !== null && search.get("open") === "true"){
-      $('#addEquipment').modal('show');
-    }
-  })
-  $('[data-toggle="tooltip"]').tooltip()
-})
--- app/assets/javascripts/questions.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(document).ready(function(){
-    let search = new URLSearchParams(document.location.search)
-    if(search.get("open") !== null && search.get("open") === "true"){
-      $('#addQuestion').modal('show');
-    }
-  })
-})
