7a5072db40c8dea61b5c8fa7c3d7503ead21011c
Norvylle Austin Uy <austin.uy@adish.co.jp>
2019-10-30 14:36:57 +0800

    added port from ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence 
+  # over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -27,30 +28,33 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system (see config/storage.yml 
+  # for options)
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {
+    host: ENV['HOST'], port: ENV['PORT']
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default(charset: 'utf-8')
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = { from: ENV["USERNAME"] }
-  
+  config.action_mailer.default_options = { from: ENV['USERNAME'] }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => ENV["SMTP_ADDRESS"],
-      :port                 => 587,
-      :domain               => 'localhost',
-      :user_name            => ENV["USERNAME"],
-      :password             => ENV["PASSWORD"],
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
+      address: ENV['SMTP_ADDRESS'],
+      port: 587,
+      domain: 'localhost',
+      user_name: ENV['USERNAME'],
+      password: ENV['PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
   }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
